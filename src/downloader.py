e10ae1fffca8814e34bffd477d42e01faf34447a
Julio Angulo <solojoas@gmail.com>
2024-05-21 17:48:55 +0200

    Going back to simple recursive approach
    I've got a lot of extrange errors using asyncio so I'm going to
    a simpler approach, maybe I could add threads later...

--- src/downloader.py
+++ src/downloader.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
-import asyncio
-import concurrent.futures
+import traceback
 from pathlib import Path
 import dropbox
 from dropbox.files import FileMetadata, FolderMetadata
@@ -19,8 +18,9 @@ def parse_arguments():
 
 def write_file(dropbox_data, file_path):
     with open(file_path, "wb") as f:
-        for chunk in dropbox_data.iter_content(chunk_size=4096):
-            f.write(chunk)
+        f.write(dropbox_data.content)
+        #for chunk in dropbox_data.iter_content(chunk_size=4096):
+        #    f.write(chunk)
 
 def download_file(dbx, dropbox_path, local_path):
     """
@@ -30,42 +30,41 @@ def download_file(dbx, dropbox_path, local_path):
     :param dropbox_path: Path to the file on Dropbox
     :param local_path: Path to save the file locally
     """
+    print(f"Downloading {dropbox_path} to {local_path}...")
     try:
-        metadata, res = dbx.files_download(path=dropbox_path)
+        _, res = dbx.files_download(path=dropbox_path)
         write_file(res, local_path)
-        print(f"Downloaded {dropbox_path} to {local_path}")
+        
     except dropbox.exceptions.ApiError as e:
         print(f"Failed to download {dropbox_path}: {e}")
 
-async def download_all_files(dbx, folder_path, base_folder, loop):
+def download_all_files(dbx, folder_path:str, base_folder:Path):
     """
-    Download all files in the specified Dropbox folder asynchronously.
+    Download all files in the specified Dropbox folder synchronously.
 
     :param dbx: Dropbox API client
     :param folder_path: Path to the folder on Dropbox
     :param base_folder: Base local folder to save downloaded files
-    :param loop: The asyncio event loop
     """
-    try:
-        result = dbx.files_list_folder(folder_path)
-        tasks = []
-        with concurrent.futures.ThreadPoolExecutor() as executor:
-            for entry in result.entries:
-                if isinstance(entry, FileMetadata):
-                    local_path = base_folder / entry.path_display.strip('/')
-                    local_path.parent.mkdir(parents=True, exist_ok=True)
-                    tasks.append(loop.run_in_executor(executor, download_file, dbx, entry.path_lower, local_path))
-                elif isinstance(entry, FolderMetadata):
-                    local_subfolder = base_folder / entry.path_display.strip('/')
-                    local_subfolder.mkdir(parents=True, exist_ok=True)
-                    tasks.append(download_all_files(dbx, entry.path_lower, base_folder, loop))
+    result = dbx.files_list_folder(folder_path)
+    
+    for entry in result.entries:
+        if isinstance(entry, FileMetadata):
+            local_path = base_folder / entry.path_display.strip('/')
+            local_path.parent.mkdir(parents=True, exist_ok=True)
+            # download file only if it does not exists
+            if local_path.exists() and local_path.stat().st_size == entry.size:
+                print(f"file {local_path} exists, download skipped")
+            else:
+                download_file(dbx, entry.path_lower, local_path)
+        
+        elif isinstance(entry, FolderMetadata):
+            local_subfolder = base_folder / entry.path_display.strip('/')
+            local_subfolder.mkdir(parents=True, exist_ok=True)
+            download_all_files(dbx, entry.path_lower, base_folder)
 
-            async with asyncio.Semaphore(10):
-                await asyncio.gather(*tasks)
-    except dropbox.exceptions.ApiError as e:
-        print(f"API Error: {e}")
 
-async def main():
+def main():
     args = parse_arguments()
 
     # Create a Dropbox client
@@ -74,11 +73,8 @@ async def main():
     # Base local folder to save downloaded files
     base_folder = Path(args.path_local)
     
-    # Get the current event loop
-    loop = asyncio.get_running_loop()
-
     # Download all files in the specified Dropbox folder
-    await download_all_files(dbx, args.dropbox_path, base_folder, loop)
+    download_all_files(dbx, args.dropbox_path, base_folder)
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
